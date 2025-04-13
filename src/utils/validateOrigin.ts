7d0e615ce22b9432e462db9994f442d16430a68f
gabriel-maggioni <devmaggioni@gmail.com>
2025-04-13 12:35:12 -0300

    fix origin

--- src/utils/validateOrigin.ts
+++ src/utils/validateOrigin.ts
@@ -1,10 +1,13 @@
  
  const validateOrigin = (req: any, reply: any, done: () => void) => {
-    const origin = req.headers.origin || req.headers.host || "";
-    if (process.env.DOMAIN && !process.env.DOMAIN.includes(origin)) {
+    const origin = req.headers.origin || req.headers.host || "*";
+    if (process.env.DOMAIN === "*"){
+      return done();
+    }
+    else if (process.env.DOMAIN && !process.env.DOMAIN.includes(origin)) {
       return reply.code(401).send({ statusCode: 401, message: "Not Authorized" });
     }
-    done();
+     return done();
   };  
 
   export default validateOrigin
